a071adfc70367a05ab8ee45c8071c9a27c6e8cf0
Ayantunde Tolu <tolextolex808@gmail.com>
2020-09-09 23:23:07 +0100

    Manactivity and Splash Activity added to the project .

--- app/src/main/java/com/ayantundetolu/aad_preproject/MainActivity.java
+++ app/src/main/java/com/ayantundetolu/aad_preproject/MainActivity.java
@@ -1,8 +1,17 @@
-package com.ayantundetolu.aad_preproject;
+package com.amrmustafa.AAD;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.amrmustafa.AAD.adapter.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        Button tbar=(Button)findViewById(R.id.btn_search);
+        tbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,FormData.class));
+            }
+        });
+}
 }
